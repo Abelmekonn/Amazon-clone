f3d507f71c41e5742ce9e85a77abe27d770651f5
Abelmekonn <Abelmekonn9@gmail.com>
2024-04-26 10:27:17 +0300

    hgjgw

--- src/Router.jsx
+++ src/Router.jsx
@@ -7,13 +7,20 @@ import Orders from './pages/Orders/Orders'
 import Cart from './pages/Cart/Cart'
 import Results from './pages/Results/Results'
 import ProductDetails from './pages/ProductDetails/ProductDetails'
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+const stripePromise = loadStripe('pk_test_51P9BEz2LYJnYFChtNYhTLEyvlKYttMuPncgtJNCaNcYNxwZeN5VmCLfUPaEOW67mA4laS7kufJNFNIzt5uKf0fpa00Wu9QaQzA');
 function Routing() {
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<Landing />} />
                 <Route path='/auth' element={<Auth />} />
-                <Route path='/payment' element={<Payment/>} />
+                <Route path='/payment' element={
+                <Elements stripe={stripePromise}>
+                    <Payment/>
+                </Elements>
+                } />
                 <Route path='/order' element={<Orders />} />
                 <Route path='/category/:categoryName' element={<Results />} />
                 <Route path='/Products/:productId' element={<ProductDetails />} />
--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,10 +1,100 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
+import LayOut from '../../components/LayOut/LayOut'
+import classes from "./Payment.module.css"
+import ProductCard from '../../components/Product/ProductCard'
+import { DataContext } from "../../components/DataProvider/DataProvider"
+import CurrencyFormat from '../../components/ConcurencyFormat/ConcurencyFormat';
+import {axiosInstance} from '../../Api/axios'
 
 function Payment() {
+    const [{ basket, user }, dispatch] = useContext(DataContext)
+    const totalItem = basket?.reduce((amount, item) => {
+        return amount + item.amount
+    }, 0)
+
+    const [cardError, setCardError] = useState("")
+    const stripe = useStripe();
+    const elements = useElements();
+    const handelChange = (e) => {
+        console.log(e)
+        e.errors?.message ? setCardError(e?.errors?.message) : setCardError('')
+    }
+    const total = basket.reduce((amount, item) => {
+        return item.price * item.amount + amount
+    }, 0)
+    const handlePayment = async (e) => {
+        e.preventDefault();
+    
+        try {
+            const response = await axiosInstance({
+                method: "POST",
+                url: `/payment/create?total=${total * 100}`
+            });
+            const clientSecret = response.data?.clientSecret;
+            // client side confirmation
+            const confirmation = await stripe.confirmCardPayment(
+                clientSecret,
+                {
+                    payment_method: {
+                        card: elements.getElement(CardElement)
+                    }
+                }
+            );
+            // Handle the payment confirmation response here
+        } catch (error) {
+            // Handle errors here
+        }
+    }
+    
     return (
-        <div>
-            Payment
-        </div>
+        <LayOut>
+            <div className={classes.payment_header}>
+                Checkout ({totalItem}) items
+            </div>
+            <section className={classes.payment}>
+                {/* Address */}
+                <div className={classes.flex}>
+                    <h3>Delivery Address</h3>
+                    <div>
+                        <div>{user.email}</div>
+                        <div>1234 Main St</div>
+                        <div>Apt 6B</div>
+                    </div>
+                </div>
+                <hr />
+                {/* product */}
+                <div className={classes.flex}>
+                    <h3>Review item and delivery</h3>
+                    <div>
+                        {
+                            basket?.map((item) => <ProductCard product={item} flex={true} />)
+                        }
+                    </div>
+                </div>
+                <hr />
+                {/* card form */}
+                <div className={classes.flex}>
+                    <h3>Payment method</h3>
+                    <div className={classes.payment_card_container}>
+                        <div className={classes.payment_details}>
+                            <form action="" onSubmit={handlePayment}>
+                                {cardError && (
+                                    <small style={{ color: "red" }}>{cardError}</small>)}
+                                <CardElement onChange={handelChange} />
+                                {/* price */}
+                                <div className={classes.payment_price}>
+                                    <div style={{display:"flex",gap:"10px"}}>
+                                        <p>total order |</p> <CurrencyFormat amount={total} />
+                                    </div>
+                                    <button type='submit'>Pay now</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </LayOut>
     )
 }
 
