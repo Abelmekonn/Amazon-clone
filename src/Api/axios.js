ad0a0a3b1bd849b27160d376f7e64c33071ab1db
Abelmekonn <Abelmekonn9@gmail.com>
2024-05-25 01:13:48 +0300

    amazon

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
 export const axiosInstance=axios.create({
-    baseURL:"http://127.0.0.1:5001/clone-dd986/us-central1/api"
+
+    // baseURL:"http://127.0.0.1:5001/clone-dd986/us-central1/api"
+
+    baseURL:"https://amazon-backend-deploy-vs97.onrender.com"
 })
